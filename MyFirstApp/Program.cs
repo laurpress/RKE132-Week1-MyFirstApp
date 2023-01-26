8c0a7c05d41eb5b5fd227c135700c4882dec409f
Laur Press <lpress@tktk.ee>
2023-01-26 18:51:46 +0200

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,13 @@
+﻿// This is a comment to my code
+
+//rakendus küsib kasutajalt sisestada tema nime
+//rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name:"); //output
+//string - sõne
+string userName = Console.ReadLine(); //Input
+
+Console.WriteLine("Hello" + ", " + userName + "!"); //output
+
+//string interpolation
+Console.WriteLine($"Hello, {userName}!"); //output
